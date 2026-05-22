524f46815c9e5eb4aad4c1764a032494ee98b4a6
Jacob Kolinski <dangkhoa.lop7b2@gmail.com>
2026-05-22 14:33:46 +0700

    Feature commit adding sfml library

--- sources/crossing.cpp
+++ sources/crossing.cpp
@@ -1,8 +1,43 @@
-#include "common.hpp"
-#include "logger.hpp"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char *argv[]) {
-  crossing::Logger::makeLog(crossing::LoggerType::DEBUG, __FILE__, __LINE__,
-                            "game start!");
-  return 0;
+int main() {
+  // Create the main window
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+
+  // Load a sprite to display
+  const sf::Texture texture("cute_image.jpg");
+  sf::Sprite sprite(texture);
+
+  // Create a graphical text to display
+  const sf::Font font("arial.ttf");
+  sf::Text text(font, "Hello SFML", 50);
+
+  // Load a music to play
+  sf::Music music("nice_music.ogg");
+
+  // Play the music
+  music.play();
+
+  // Start the game loop
+  while (window.isOpen()) {
+    // Process events
+    while (const std::optional event = window.pollEvent()) {
+      // Close window: exit
+      if (event->is<sf::Event::Closed>())
+        window.close();
+    }
+
+    // Clear screen
+    window.clear();
+
+    // Draw the sprite
+    window.draw(sprite);
+
+    // Draw the string
+    window.draw(text);
+
+    // Update the window
+    window.display();
+  }
 }
